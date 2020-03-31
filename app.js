ffc8c23567b29edd91dbbbbe8505ae4701250c2b
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-03-31 12:25:54 -0700

    Added search form

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
+    console.log(req.query) // => "{ term: hey" }
+    res.render('home')
+  })
+
+app.get('/', (req, res) => {
   // set the url of the gif
   const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
   // render the hello-gif view, passing the gifUrl into the view to be displayed
@@ -24,6 +29,8 @@ app.get('/greetings/:name', (req, res) => {
   // render the greetings view, passing along the name
   res.render('greetings', { name });
 })
+
+
 
 // Start Server
 
