a28477156602a238df8a4e9b31f6389c111419df
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-03-31 22:45:35 -0700

    Added working gif search

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // Require Libraries
 const express = require('express');
 
+// Require tenorjs near the top of the file
+  const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "FGUMZP8TJDG6", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
 // App Setup
 const app = express();
 
@@ -11,25 +19,21 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
-app.get('/', (req, res) => {
-    console.log(req.query) // => "{ term: hey" }
-    res.render('home')
+  app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
   })
-
-app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
-})
-
-app.get('/greetings/:name', (req, res) => {
-  // grab the name from the path provided
-  const name = req.params.name;
-  // render the greetings view, passing along the name
-  res.render('greetings', { name });
-})
-
 
 
 // Start Server
